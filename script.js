6fe2b3021db47fa9edfbab1167ef91d1e45cab6a
Alberto Duarte <betodute@gmail.com>
2022-06-07 15:06:21 -0700

    JQuery Fetch From API Working
    Very cool I am able to post and get next step is to delete.

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-var createNewTask = function (userInput) {
+var postNewTask = function (userInput) {
+  $.ajax({
+    type: 'POST',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=389',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      task: {
+        content: `${userInput}`
+      }
+    }),
+    success: function (response, textStatus) {
+      printNewTask(response.task.content);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
+var printNewTask = function (userInput) {
  var taskDiv = $(document.createElement('div'));
  taskDiv.addClass("col-12 m-1")
 
@@ -8,7 +28,30 @@ var createNewTask = function (userInput) {
  $('#userInput').val('')
 }
 
+var getTasks = function (response) {
+  $(response.tasks).each(function(index, element) {
+    printNewTask(element.content)
+  })
+}
+
 $(document).ready(function () {
+
+  // CHECK API FOR EXISTING TASKS
+
+  var checkAPI = $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=389',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      getTasks(response)
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+
+
+  // NEW TASK
 
   $('#newTask').on('submit', function(event){
     event.preventDefault();
@@ -17,7 +60,8 @@ $(document).ready(function () {
     // The following ternary filters out blank inputs
 
     userInput ?
-    createNewTask(userInput) :
+    postNewTask(userInput) :
     alert("Cannot Submit Blank Input")
+
   })
 })
